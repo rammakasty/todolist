f68e177c14d71fca6aeaec818244ff79d9adc93b
choi ha young <gkdudk15@naver.com>
2023-02-09 13:20:58 +0900

    feat: 과제 테스트

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import React, { useState } from 'react';
-import './App.css';
-import TodoBoard from './components/TodoBoard';
+import React from 'react';
 
-function App() {
-    const [inputValue, setInputValue] = useState('');
-    const [todoList, setTodoList] = useState([]);
-    const addItem = () => {
-        console.log('im hererere!', inputValue);
-        setTodoList([...todoList, inputValue]);
+const App = () => {
+    const style = {
+        padding: '100px',
+        display: 'flex',
+        gap: '12px',
     };
-    return (
-        <main>
-            <input type="text" onChange={(event) => console.log(event.target.value)} />
-            <button onClick={addItem}>추가</button>
 
-            <TodoBoard />
-        </main>
+    const squareStyle = {
+        width: '100px',
+        height: '100px',
+        border: '1px solid green',
+        borderRadius: '10px',
+        display: 'flex',
+        alignItems: 'center',
+    };
+
+    const [todoList, setTodoList] = useState([
+        { id: 0, title: 'react를 배워볼까요', isDone: false },
+    ]);
+
+    const [todoInputTitle, setTodoInputTitle] = useState('');
+    const [todoInputDesc, setTodoInputDesc] = useState('');
+    const titleChangeHandler = (e) => setTodoInputTitle(e.target.value);
+    const descChangeHandler = (e) => setTodoInputDesc(e.target.value);
+
+    const submitBtnHandler = () => {
+        const newObj = {
+            id: Date.now(),
+            title: todoInputTitle,
+            desc: todoInputDesc,
+            isDone: false,
+        };
+        setTodoList([...todoList, newObj]);
+        setTodoInputTitle('');
+        setTodoInputDesc('');
+    };
+
+    return (
+        <div style={style}>
+            <div style={squareStyle}>react를 배워봅시다.</div>
+            <input value={todoInputDesc} onChange={descChangeHandler} />
+            <button onClick={submitBtnHandler}>추가</button>
+        </div>
     );
-}
+};
 
 export default App;
--- src/components/TodoItem.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function TodoItem() {
-    return <div calssName="todo-item">공부하기</div>;
-}
-
-export default TodoItem;
